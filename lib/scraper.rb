183c63d4898a39d032dbbc8bb0e60dc453c01710
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-30 19:43:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,25 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
+#attr_accessor :name, :location, :profile_url, :index_url
+
+#@@all = []
 
   def self.scrape_index_page(index_url)
-    
+    student_site = Nokogiri::HTML(open(index_url))
+
+    students = []
+
+    student_site.css(".student-card").each do |student|
+      
+      binding.pry
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
